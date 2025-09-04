8375a0055a1f89c056e1f8ce5586949a4e415b43
VICTOR <victor.cai@oocl.com>
2025-09-04 19:02:20 +0800

    refactor: rename ANY_SAPCE_SEPARATOR to ANY_SPACE_SEPARATOR

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -3,15 +3,15 @@ import java.util.Arrays;
 import java.util.stream.Collectors;
 
 public class WordFrequencyGame {
-    public static final String ANY_SAPCE_SEPARATOR = "\\s+";
+    public static final String ANY_SPACE_SEPARATOR = "\\s+";
 
     public String getResult(String inputStr) {
-        if (inputStr.split(ANY_SAPCE_SEPARATOR).length == 1) {
+        if (inputStr.split(ANY_SPACE_SEPARATOR).length == 1) {
             return inputStr + " 1";
         } else {
             try {
                 //split the input string with 1 to n pieces of spaces
-                String[] words = inputStr.split(ANY_SAPCE_SEPARATOR);
+                String[] words = inputStr.split(ANY_SPACE_SEPARATOR);
 
                 List<Input> frequencies = countFrequencies(words);
 
