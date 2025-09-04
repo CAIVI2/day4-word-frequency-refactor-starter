a57930df6febaea67ff8b9f640d8288c1d93a58c
VICTOR <victor.cai@oocl.com>
2025-09-04 18:56:05 +0800

    refactor: Replace Loop with Pipeline (Stream API)

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -3,6 +3,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.StringJoiner;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class WordFrequencyGame {
     public static final String ANY_SAPCE_SEPARATOR = "\\s+";
@@ -27,23 +29,16 @@ public class WordFrequencyGame {
     }
 
     private static String composeOutput(List<Input> frequencies) {
-        StringJoiner joiner = new StringJoiner("\n");
-        for (Input w : frequencies) {
-            String s = w.getValue() + " " + w.getWordCount();
-            joiner.add(s);
-        }
-        return joiner.toString();
+        return frequencies.stream()
+                .map(w -> w.getValue() + " " + w.getWordCount())
+                .collect(java.util.stream.Collectors.joining("\n"));
     }
 
     private List<Input> countFrequencies(String[] words) {
-        Map<String, Integer> frequencyMap = new HashMap<>();
-        for (String word : words) {
-            frequencyMap.put(word, frequencyMap.getOrDefault(word, 0) + 1);
-        }
-        List<Input> frequencies = new ArrayList<>();
-        for (Map.Entry<String, Integer> entry : frequencyMap.entrySet()) {
-            frequencies.add(new Input(entry.getKey(), entry.getValue()));
-        }
-        return frequencies;
+        return Arrays.stream(words)
+                .collect(Collectors.groupingBy(word -> word, java.util.stream.Collectors.counting()))
+                .entrySet().stream()
+                .map(entry -> new Input(entry.getKey(), entry.getValue().intValue()))
+                .collect(java.util.stream.Collectors.toList());
     }
 }
